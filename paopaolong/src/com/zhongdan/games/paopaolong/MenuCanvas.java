e2873a73dc214c567fba36485f544ee41c6fa4bb
Lufei Deng <denglufei@gmail.com>
2017-07-24 21:32:49 -0400

    paopaolong: add menu

---
+++ paopaolong/src/com/zhongdan/games/paopaolong/MenuCanvas.java
@@ -0,0 +1,94 @@
+package com.zhongdan.games.paopaolong;
+
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.game.GameCanvas;
+import javax.microedition.lcdui.game.LayerManager;
+import javax.microedition.lcdui.game.Sprite;
+import javax.microedition.lcdui.game.TiledLayer;
+
+import com.zhongdan.games.paopaolong.MyGameConstants.Menu;
+
+public class MenuCanvas extends GameCanvas {
+
+	private MainMidlet midlet;
+	private Graphics graphics = this.getGraphics();
+	private LayerManager layerManager = new LayerManager();
+	private TiledLayer backgroundLayer;
+	private Image backgroundImg;
+	private Image startGameBtnImg;
+	private Image endGameBtnImg;
+	private Sprite startGameBtnSprite;
+	private Sprite endGameBtnSprite;
+	private Sprite topBtnSprite;
+	private Sprite botBtnSprite;
+	private int selectedItem = 1;
+
+	protected MenuCanvas(MainMidlet midlet) {
+		super(false);
+		this.midlet = midlet;
+		this.setFullScreenMode(true);
+		initCanvas();
+	}
+
+	private void initCanvas() {
+		// Initialize background
+		backgroundImg = ImageUtil.createImage("/background.png");
+		backgroundLayer = new TiledLayer(1, 1, backgroundImg, backgroundImg.getWidth(), backgroundImg.getHeight());
+		backgroundLayer.setCell(0, 0, 1);
+		layerManager.append(backgroundLayer);
+
+		// Initialize buttons
+		startGameBtnImg = ImageUtil.createImage("/menu_btn_start.png");
+		endGameBtnImg = ImageUtil.createImage("/menu_btn_quit.png");
+		startGameBtnSprite = new Sprite(startGameBtnImg, 187, 75);
+		endGameBtnSprite = new Sprite(endGameBtnImg, 187, 75);
+		startGameBtnSprite.setFrame(0);
+		endGameBtnSprite.setFrame(0);
+		topBtnSprite = startGameBtnSprite;
+		botBtnSprite = endGameBtnSprite;
+		topBtnSprite.setPosition(Menu.BTN_TOP_X, Menu.BTN_TOP_Y);
+		botBtnSprite.setPosition(Menu.BTN_BOT_X, Menu.BTN_BOT_Y);
+		topBtnSprite.setFrame(1);
+		layerManager.insert(startGameBtnSprite, 0);
+		layerManager.insert(endGameBtnSprite, 0);
+
+		// Paint map
+		layerManager.paint(graphics, 0, 0);
+		this.flushGraphics();
+	}
+
+	private void updateSelectedButton() {
+		topBtnSprite.setFrame(0);
+		botBtnSprite.setFrame(0);
+		if (selectedItem == 1) {
+			topBtnSprite.setFrame(1);
+		} else if (selectedItem == 2) {
+			botBtnSprite.setFrame(1);
+		}
+		layerManager.paint(graphics, 0, 0);
+		this.flushGraphics();
+	}
+
+	protected void keyPressed(int keyCode) {
+		if (keyCode == Constants.KeyCode.UP) {
+			if (selectedItem > 1) {
+				selectedItem--;
+				updateSelectedButton();
+			}
+		} else if (keyCode == Constants.KeyCode.DOWN) {
+			if (selectedItem < 2) {
+				selectedItem++;
+				updateSelectedButton();
+			}
+		} else if (keyCode == Constants.KeyCode.OK) {
+			if (selectedItem == 1) {
+				this.midlet.getMyGameCanvas().initCanvas();
+				this.midlet.getDisplay().setCurrent(this.midlet.getMyGameCanvas());
+			} else if (selectedItem == 2) {
+				this.midlet.notifyDestroyed();
+			}
+		}
+	}
+
+}
