2e5451d39ec33b2a210a7bb4ae2a79101abf5787
Lufei Deng <denglufei@gmail.com>
2017-07-30 04:06:46 -0400

    sokoban: remove level select button

--- sokoban/src/com/zhongdan/games/sokoban/GameConstants.java
+++ sokoban/src/com/zhongdan/games/sokoban/GameConstants.java
@@ -9,11 +9,13 @@ public class GameConstants {
 
 	public static class Menu {
 		public static int BTN_TOP_X = 220;
-		public static int BTN_TOP_Y = 220;
+		// public static int BTN_TOP_Y = 220;
+		public static int BTN_TOP_Y = 240;
 		public static int BTN_MID_X = 220;
 		public static int BTN_MID_Y = 280;
 		public static int BTN_BOT_X = 220;
-		public static int BTN_BOT_Y = 340;
+		public static int BTN_BOT_Y = 320;
+		// public static int BTN_BOT_Y = 340;
 	}
 
 	public static class GameSettings {
